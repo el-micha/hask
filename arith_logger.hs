13bb7094287b0eb8e0216d7da1b5d21b67fa94b2
Michael Weigelt <michael.weigelt@bluewin.ch>
2021-07-27 22:47:28 +0200

    logging with writerT

--- arith_logger.hs
+++ arith_logger.hs
@@ -1,6 +1,6 @@
 import Control.Monad.Identity
 import Control.Monad.Error
-
+import Control.Monad.Writer
 
 -- evaluate arithmetic with logging
 
@@ -25,6 +25,7 @@ eval1 (Minus x y) = do x1 <- eval1 x
 
 t1 = Minus (Lit 8) (Lit 4)
 t2 = Minus (Lit 4) (Lit 8)
+t3 = Plus (Minus (Lit 3) (Lit 4)) (Plus (Minus (Lit 5) (Lit 6)) (Lit 9))
 -- runIdentity $ eval1 t
 
 eval2 :: Expr -> ErrorT String Identity Int
@@ -39,6 +40,45 @@ eval2 (Minus x y) = do x1 <- eval2 x
                          False -> return (x1 - x2)
 
 -- runIdentity $ runErrorT $ eval2 t2
+
+eval3 :: Expr -> WriterT [String] Identity Int
+eval3 (Lit x) = do tell ["literal " ++ show x]
+                   return x
+eval3 (Plus x y) = do tell [show x ++ " (+) " ++ show y]
+                      x1 <- eval3 x
+                      y1 <- eval3 y
+                      tell [show x1 ++ " + " ++ show y1]
+                      return (x1 + y1)
+eval3 (Minus x y) = do tell [show x ++ " (-) " ++ show y]
+                       x1 <- eval3 x
+                       y1 <- eval3 y
+                       tell [show x1 ++ " - " ++ show y1]
+                       return (x1 - y1)
+
+-- data Expr = Lit Int | Plus Expr Expr | Minus Expr Expr deriving Show
+-- t3 = Plus (Minus (Lit 3) (Lit 4)) (Plus (Minus (Lit 5) (Lit 6)) (Lit 9))
+
+-- runIdentity $ runWriterT $ eval3 t3
+-- ["Minus (Lit 3) (Lit 4) (+) Plus (Minus (Lit 5) (Lit 6)) (Lit 9)",
+-- "Lit 3 (-) Lit 4",
+-- "literal 3",
+-- "literal 4",
+-- "3 - 4",
+-- "Minus (Lit 5) (Lit 6) (+) Lit 9",
+-- "Lit 5 (-) Lit 6",
+-- "literal 5",
+-- "literal 6",
+-- "5 - 6",
+-- "literal 9",
+-- "-1 + 9",
+-- "-1 + 8"]
+
+
+
+
+
+
+
 
 
 
